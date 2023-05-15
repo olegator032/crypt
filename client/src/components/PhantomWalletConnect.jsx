688fc040aa18e0087a4c7de9d686fae4454257ac
olegator032 <olegpalashchenko@gmail.com>
2023-05-16 00:48:06 +0300

    base

---
+++ client/src/components/PhantomWalletConnect.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+
+export const Phantom = () => {
+  // State
+  const [walletAddress, setWalletAddress] = useState(null);
+  
+
+  // Actions
+  const checkIfWalletIsConnected = async () => {
+    if (window?.solana?.isPhantom) {
+      console.log('Phantom wallet found!');
+      const response = await window.solana.connect({ onlyIfTrusted: true });
+      console.log(
+        'Connected with Public Key:',
+        response.publicKey.toString()
+      );
+       const pubKey = response.publicKey.toString();
+
+      /*
+       * Set the user's publicKey in state to be used later!
+       */
+      setWalletAddress(pubKey);
+    } else {
+      alert('Solana object not found! Get a Phantom Wallet 👻');
+    }
+  };
+
+  const connectWallet = async () => {
+    const { solana } = window;
+  
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  // UseEffects
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+  return (
+    <div className="Wallet">
+			{!walletAddress && renderNotConnectedContainer() || walletAddress}
+       </div>
+  );
+};
